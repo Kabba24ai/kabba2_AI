0461daafe22ef64947ec9a5edaf31b499f88af94
Raj Chotaliya <114693815+RajChotaliya@users.noreply.github.com>
2025-07-03 15:14:44 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,72 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import autoprefixer from 'autoprefixer';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+export default defineConfig(() => {
+    const hmrHost = process.env.VITE_HMR_HOST || 'kabba.local';
+
+    return {
+        server: {
+            host: '0.0.0.0',
+            port: 8000,
+            strictPort: true,
+            origin: `http://${hmrHost}:8000`,
+            cors: true,
+            hmr: {
+                host: hmrHost,
+            },
+            // fs: {
+            //     strict: false,
+            //     allow: [".."],
+            // },
+        },
+        build: {
+            sourcemap: false, // no source maps in production
+            minify: "terser",
+            terserOptions: {
+                compress: {
+                    drop_console: false, // change to true to remove console.logs
+                    drop_debugger: true,
+                },
+                format: {
+                    comments: false,
+                },
+            },
+        },
+        css: {
+            devSourcemap: false, // no CSS maps in dev
+        },
+        optimizeDeps: {
+            exclude: [
+                // exclude css or js should be list here
+            ],
+            include: [],
+        },
+        //logLevel: "info",
+        plugins: [
+            tailwindcss(),
+            laravel({
+                input: [
+                    // Default
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+
+                    // If you have global/shared assets, list here
+                    // 'resources/shared/css/shared.css',
+                    // 'resources/shared/js/shared.js',
+
+                    // Admin entry points
+                    'resources/admin/css/app.css',
+                    'resources/admin/js/app.js',
+
+                    // Front entry points
+                    "resources/front/assets/css/app.css",
+                    "resources/front/assets/js/app.js",
+                    "resources/front/assets/js/products/product-details-page.js"
+                ],
+                refresh: true,
+            }),
+        ],
+    };
 });

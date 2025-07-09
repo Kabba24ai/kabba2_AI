9c4e3fd684e16a0414f60d48fb92f7558e022060
Raj Chotaliya <rajkc.webdev@gmail.com>
2025-07-09 15:12:39 +0530

    Tax exempt at a top

--- resources/views/front/checkout/index.blade.php
+++ resources/views/front/checkout/index.blade.php
@@ -97,6 +97,12 @@
                             $primaryAddress = null;
                         @endphp
                     @endauth
+                    <!-- Tax Exempt -->
+                    <div class="flex items-center gap-2">
+                        <input id="taxExempt" type="checkbox" class="accent-blue-500 h-4 w-4" />
+                        <label for="taxExempt" class="text-sm">Tax Exempt</label>
+                    </div>
+
                     <div id="billingDiv" class="space-y-4 {{ $primaryAddress ? 'hidden' : '' }}">
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-2 ">
                             <div>
@@ -430,11 +436,7 @@
                         @enderror
                     </div>
 
-                    <!-- Tax Exempt -->
-                    <div class="flex items-center gap-2">
-                        <input id="taxExempt" type="checkbox" class="accent-blue-500 h-4 w-4" />
-                        <label for="taxExempt" class="text-sm">Tax Exempt</label>
-                    </div>
+
 
                     <!-- Payment Method -->
                     <div class="mx-auto" id="paymentForm">

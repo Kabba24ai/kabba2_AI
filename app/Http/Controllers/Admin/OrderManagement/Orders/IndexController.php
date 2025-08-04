7f3f8367d5793d65bb5952be2f7baf7c7cfffb70
Raj Chotaliya <rajkc.webdev@gmail.com>
2025-08-04 19:38:34 +0530

    Pagination updated

--- app/Http/Controllers/Admin/OrderManagement/Orders/IndexController.php
+++ app/Http/Controllers/Admin/OrderManagement/Orders/IndexController.php
@@ -62,7 +62,7 @@ class IndexController extends Controller
             });
         }
 
-        $orders = $query->latest('id')->paginate(1)->withQueryString(); // keeps filters in pagination links
+        $orders = $query->latest('id')->paginate(10)->withQueryString(); // keeps filters in pagination links
 
         // Return only the table partial if it's an AJAX request
         if ($request->ajax()) {

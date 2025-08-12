c349f8fa5b7dd3344433886ef3cadb9580a10fb3
Raj Chotaliya <rajkc.webdev@gmail.com>
2025-08-12 18:54:00 +0530

    Order list api filters add

--- app/Http/Controllers/Api/Admin/V1/Orders/IndexController.php
+++ app/Http/Controllers/Api/Admin/V1/Orders/IndexController.php
@@ -27,20 +27,43 @@ class IndexController extends BaseController
         $validatedData = $request->validated();
 
         $perPage = $validatedData['per_page'] ?? 10;
-        $status  = $validatedData['status'] ?? null;
+        $status = $validatedData['status'] ?? null;
+        $paymentMethod = $validatedData['payment_method'] ?? null;
+        $search = $validatedData['search'] ?? null;
+        $categoryId = $validatedData['category_id'] ?? null;
 
-        $orders = Order::query()->with('shippingAddress', 'products.product', 'lastPayment')
-        ->when($status && $status !== 'All', fn($query) => $query->whereHas('lastPayment', function ($q) use ($status) {
-            $q->where('status', $status);
-        }))
-        ->orderByDesc('id')
-        ->paginate($perPage);
+        $orders = Order::query()
+            ->with('shippingAddress', 'products.product', 'lastPayment')
+            ->when(
+                $status && $status !== 'All',
+                fn($query) => $query->whereHas('lastPayment', function ($q) use ($status) {
+                    $q->where('status', $status);
+                }),
+            )
+            ->when(
+                $search,
+                fn($query) => $query->whereHas('shippingAddress', function ($q) use ($search) {
+                    $q->where('first_name', 'like', '%' . $search . '%')->orWhere('last_name', 'like', '%' . $search . '%');
+                }),
+            )
+            ->when(
+                $categoryId,
+                fn($query) => $query->whereHas('products.product.categories', function ($q) use ($categoryId) {
+                    $q->where('product_categories.id', $categoryId);
+                }),
+            )
+            ->when($paymentMethod && $paymentMethod !== 'All', fn($query) => $query->whereHas('lastPayment', fn($q) => $q->where('payment_method', $paymentMethod)))
+            ->orderByDesc('id')
+            ->paginate($perPage);
 
         if ($orders->isEmpty()) {
-            return response()->json([
-                'success' => false,
-                'message' => trans('messages.api.admin.v1.orders.no_orders_found'),
-            ], JsonResponse::HTTP_NOT_FOUND);
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => trans('messages.api.admin.v1.orders.no_orders_found'),
+                ],
+                JsonResponse::HTTP_NOT_FOUND,
+            );
         }
 
         return response()->json([
@@ -49,9 +72,9 @@ class IndexController extends BaseController
             'orders' => ListResource::collection($orders),
             'pagination' => [
                 'current_page' => $orders->currentPage(),
-                'last_page'    => $orders->lastPage(),
-                'per_page'     => $orders->perPage(),
-                'total'        => $orders->total(),
+                'last_page' => $orders->lastPage(),
+                'per_page' => $orders->perPage(),
+                'total' => $orders->total(),
             ],
         ]);
     }
--- app/Http/Requests/Api/Admin/V1/Orders/IndexRequest.php
+++ app/Http/Requests/Api/Admin/V1/Orders/IndexRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Api\Admin\V1\Orders;
 
+use App\Enums\Orders\OrderPaymentMethod;
+use App\Enums\Orders\OrderPaymentStatus;
 use App\Http\Requests\ApiBaseFormRequest;
 
 class IndexRequest extends ApiBaseFormRequest
@@ -15,9 +17,13 @@ class IndexRequest extends ApiBaseFormRequest
     public function rules(): array
     {
         return [
-            'status' => 'required|string|in:All,Pending,Paid,Account,Partial Refund,Refunded,Failed', // Required status parameter
+            'status' => 'required|string|in:All,' . implode(',', OrderPaymentStatus::getValues()),
             'per_page' => 'nullable|integer|min:1', // Optional pagination parameter
             'page' => 'nullable|integer|min:1', // Optional page parameter
+            'search' => 'nullable|string|max:255', // Optional search parameter
+            'category_id' => 'nullable|integer|exists:product_categories,id',
+            'payment_method' => 'nullable|string|in:All,'. implode(',', OrderPaymentMethod::getValues()), // Enum validation for media type
+
         ];
     }
 
@@ -31,7 +37,7 @@ class IndexRequest extends ApiBaseFormRequest
         return [
             'status' => [
                 'description' => 'The status to filter orders. Can be either "All", "Pending", "Paid", "Account", "Partial Refund", "Refunded", or "Failed".',
-                'example' => 'Pending',
+                'example' => 'All',
                 'type' => 'string',
             ],
             'per_page' => [
@@ -43,6 +49,26 @@ class IndexRequest extends ApiBaseFormRequest
                 'description' => 'The page number for pagination.',
                 'example' => 1,
                 'type' => 'integer',
+            ],
+            'search' => [
+                'description' => 'The search term to filter orders upon the customer name.',
+                'example' => 'Raj',
+                'type' => 'string',
+            ],
+            'category_id' => [
+                'description' => 'The ID of the category to filter orders.',
+                'example' => 1,
+                'type' => 'integer',
+            ],
+            'payment_method' => [
+                'description' => 'The payment method to filter orders.',
+                'example' => 'All',
+                'type' => 'string',
+            ],
+            'payment_status' => [
+                'description' => 'The payment status to filter orders.',
+                'example' => 'Pending',
+                'type' => 'string',
             ],
         ];
     }

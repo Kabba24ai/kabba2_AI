b90b7b819ab4aba89771de6ca139fada4601258d
Raj Chotaliya <rajkc.webdev@gmail.com>
2025-09-16 23:49:21 +0530

    migration bug fixed

--- database/migrations/orders/2025_09_12_190645_change_foreign_key_to_order_products_table.php
+++ database/migrations/orders/2025_09_12_190645_change_foreign_key_to_order_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('order_products', function (Blueprint $table) {
-            //$table->dropForeign(['product_id']);
+            $table->dropForeign(['product_id']);
             $table->unsignedBigInteger('product_id')->nullable()->change();
             $table->foreign('product_id')
                 ->references('id')->on('products')

69c7bb66d6474574fcb6108b53fefb980d362ade
Raj Chotaliya <rajkc.webdev@gmail.com>
2025-09-24 19:45:30 +0530

    Job Time change for the test

--- routes/console.php
+++ routes/console.php
@@ -10,14 +10,14 @@ Artisan::command('inspire', function () {
 
 // Schedule your job
 Schedule::job(new \App\Jobs\SendDayBeforeRentalReminderJob())
-    ->dailyAt('07:00')
+    ->dailyAt('15:00')
     ->timezone('America/Chicago')
     ->withoutOverlapping()
     ->onOneServer()
     ->name('send-day-before-rental-reminder-job');
 
 Schedule::job(new SendSameDayRentalReminderJob)
-    ->dailyAt('07:00')
+    ->dailyAt('09:30')
     ->timezone('America/Chicago')
     ->withoutOverlapping()
     ->onOneServer()

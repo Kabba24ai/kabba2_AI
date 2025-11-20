9eea61d372d29b59be70a661949e97afea842970
Nipa Soni <nipavadhavana@gmail.com>
2025-11-20 17:05:03 +0530

    design changes
    design changes

--- resources/views/front/faqs/index.blade.php
+++ resources/views/front/faqs/index.blade.php
@@ -229,9 +229,9 @@
         </div>
 
     </div> */ ?>
-<div class="max-w-3xl mx-auto space-y-4 px-4 sm:px-6 lg:px-8 py-6 pb-20">
+<div class="max-w-3xl mx-auto space-y-6 px-4 sm:px-6 lg:px-8 py-6 pb-20">
     @foreach($categories as $category)
-    <div class="bg-white rounded-xl shadow-sm border border-gray-200 overflow-hidden">
+    <div class="bg-white rounded-xl shadow-sm border border-gray-300 overflow-hidden">
         <button class="category-toggle w-full flex justify-between items-center p-4 text-left cursor-pointer">
             <div class="flex items-start gap-3">
                 <div class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-50 border border-gray-200 overflow-hidden">
@@ -260,9 +260,9 @@
             </div>
         </button>
 
-        <div class="category-content {{ $category->default_expand ? '' : 'hidden' }} border-t border-gray-100">
+        <div class="category-content {{ $category->default_expand ? '' : 'hidden' }} border-t-2 border-gray-300">
             @forelse($category->questions as $question)
-            <div class="px-6 py-2 border-b border-gray-100">
+            <div class="px-6 py-2 @if(!$loop->last) border-b-2 border-gray-300 @endif">
                 <button id="faq-{{$question->unique_id}}" class="faq-toggle flex justify-between items-center w-full text-left font-medium text-sm text-gray-900 hover:text-blue-600 cursor-pointer">
                     {{ $question->question_name }}
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400  transform transition-transform duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -272,12 +272,13 @@
 
                 <div class="faq-content hidden text-gray-700 text-sm leading-relaxed mt-3">
                     <p class="text-gray-700 leading-relaxed prose prose-sm max-w-none">{!! $question->answer !!}</p>
-                    <hr class="my-3 border-gray-200" />
+                    
 
 
 
                     @if($question->related)
-                    <div class="border-t border-gray-200 pt-3">
+                    <hr class="my-4 border-gray-300" />
+                    <div class=" pt-3">
                         <p class="flex items-center gap-1 font-medium text-gray-900 text-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lightbulb w-4 h-4 text-yellow-500 mr-2">
                                 <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5"></path>

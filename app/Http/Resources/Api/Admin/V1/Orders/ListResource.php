cd2ebe3f154aaa6cbfec5560768700cfc1f0b7ac
Raj Chotaliya <rajkc.webdev@gmail.com>
2025-12-23 15:01:17 +0530

    Listresource update

--- app/Http/Resources/Api/Admin/V1/Orders/ListResource.php
+++ app/Http/Resources/Api/Admin/V1/Orders/ListResource.php
@@ -46,7 +46,15 @@ class ListResource extends JsonResource
             'delivery_address' => new OrderAddressesListResource($this->whenLoaded('shippingAddress') ?? []),
             'billing_address' => new OrderAddressesListResource($this->whenLoaded('billingAddress') ?? []),
 
-            'is_same_as_billing' => $this->whenLoaded('shippingAddress')->isSameAs($this->whenLoaded('billingAddress') ?? null) ?? false,
+            'is_same_as_billing' => (
+                $this->resource->relationLoaded('shippingAddress')
+                && $this->resource->relationLoaded('billingAddress')
+                && $this->shippingAddress
+                && $this->billingAddress
+            )
+                ? $this->shippingAddress->isSameAs($this->billingAddress)
+                : false,
+
 
             'license' => OrderMediasListResource::collection($this->whenLoaded('licenseMedia') ?? []),
             'order_products' => OrderProductsListResource::collection($this->whenLoaded('products')) ?? [],

f9be53a958ea81adbc593fc7b66a61507052f982
Raj Chotaliya <rajkc.webdev@gmail.com>
2026-02-02 19:05:05 +0530

    - disabled technician and location

--- resources/views/admin/order_management/orders/edit.blade.php
+++ resources/views/admin/order_management/orders/edit.blade.php
@@ -675,7 +675,7 @@
                                                     for="delivery_store_id_{{ $orderProduct->unique_id }}">Location</label>
                                                 <select id="delivery_store_id_{{ $orderProduct->unique_id }}"
                                                     class="delivery_store_id border rounded px-3 py-3 text-xs w-full">
-                                                    <option value="">Select Location</option>
+                                                    <option value="" disabled>Select Location</option>
                                                     @foreach ($stores as $storeItem)
                                                         <option value="{{ $storeItem->id }}"
                                                             {{ $orderProduct->delivery_store_id == $storeItem->id ? 'selected' : '' }}>
@@ -690,7 +690,7 @@
                                                     for="delivery_by_{{ $orderProduct->unique_id }}">Technician</label>
                                                 <select id="delivery_by_{{ $orderProduct->unique_id }}"
                                                     class="delivery_by border rounded px-3 py-3 text-xs w-full">
-                                                    <option value="">Select Technician</option>
+                                                    <option value="" disabled>Select Technician</option>
                                                     @foreach ($employees as $employee)
                                                         <option value="{{ $employee->id }}"
                                                             {{ $orderProduct->delivery_by == $employee->id ? 'selected' : '' }}>
@@ -801,7 +801,7 @@
                                                     for="pickup_store_id_{{ $orderProduct->unique_id }}">Location</label>
                                                 <select id="pickup_store_id_{{ $orderProduct->unique_id }}"
                                                     class="pickup_store_id border rounded px-3 py-3 text-xs w-full">
-                                                    <option value="">Select Location</option>
+                                                    <option value="" disabled>Select Location</option>
                                                     @foreach ($stores as $storeItem)
                                                         <option value="{{ $storeItem->id }}"
                                                             {{ $orderProduct->pickup_store_id == $storeItem->id ? 'selected' : '' }}>
@@ -816,7 +816,7 @@
                                                     for="pickup_by_{{ $orderProduct->unique_id }}">Technician</label>
                                                 <select id="pickup_by_{{ $orderProduct->unique_id }}"
                                                     class="pickup_by border rounded px-3 py-3 text-xs w-full">
-                                                    <option value="">Select Technician</option>
+                                                    <option value="" disabled>Select Technician</option>
                                                     @foreach ($employees as $employee)
                                                         <option value="{{ $employee->id }}"
                                                             {{ $orderProduct->pickup_by == $employee->id ? 'selected' : '' }}>

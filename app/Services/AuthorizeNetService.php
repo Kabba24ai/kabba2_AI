4381501ef4a2589a75f93848a8ab72cd73a99a4a
Raj Chotaliya <rajkc.webdev@gmail.com>
2026-02-27 20:05:52 +0530

    - authorize service update

--- app/Services/AuthorizeNetService.php
+++ app/Services/AuthorizeNetService.php
@@ -171,11 +171,15 @@ class AuthorizeNetService
                 $existingProfileId = $this->findExistingCustomerProfileId($uniqueId);
                 logger()->error("Duplicate customer profile detected for uniqueId {$uniqueId}. Existing profile ID: {$existingProfileId}");
                 if ($existingProfileId) {
-                    $updatedRows = Customer::query()->where('unique_id', $uniqueId)->update(['authorize_profile_id' => $existingProfileId]);
-                    logger()->info("Tried to update customer record with unique_id {$uniqueId} to Authorize.Net profile ID: {$existingProfileId}. Rows affected: {$updatedRows}");
-                    if ($updatedRows === 0) {
-                        logger()->warning("No customer record found with unique_id {$uniqueId} to update authorize_profile_id.");
+                    if($customer = Customer::where('unique_id', trim($uniqueId))->first()) {
+                        logger()->info("Customer record found for unique_id {$uniqueId}. Current authorize_profile_id: {$customer->authorize_profile_id}");
+
+                        $customer->authorize_profile_id = $existingProfileId;
+                        $customer->save();
+                    }else {
+                        logger()->warning("No customer record found for unique_id {$uniqueId} while handling duplicate profile error.");
                     }
+
                 }
             }
             throw new \Exception('Failed to create customer profile: ' . $error);
